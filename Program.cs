d4d5ea2451a0d807532142340b37d7487678c232
Eden Gwenda <ST10452860@rcconnect.edu.za>
2025-11-22 16:21:10 +0200

    Editted the Views while using Entity Framework Core

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
+using ContractMonthlyClaimSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+
+// Add EF Core DbContext (SQL LocalDB)
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(conn));
+
+var app = builder.Build();
+
+// Middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
